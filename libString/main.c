abe566ef4b9a45bc887beb7f63ddea5cb3153fec
Cogitary0 <84321459+Cogitary0@users.noreply.github.com>
2024-08-23 15:21:23 +0400

    Update main.c

--- libString/main.c
+++ libString/main.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include "libstring.h"
 
-
-
-
 int main() {
 
-	String_t* str = string_init("hel2lo");
-	String_t* str2 = string_init("hello");
+	String_t* str = string_init("HelloWorld");
+	String_t* str2 = string_init("ello");
+	// string_replace_all_char(str, 'l', '0');
 
-	printf("%i\n", string_eq(str,str2));
+	printf("%i\n", string_find_string(str, str2));
 
 	string_free(str);
 	string_free(str2);
-
 	return 0;
 }
